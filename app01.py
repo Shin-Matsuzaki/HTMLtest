6420191805800ef44641c90f2ce609e380bc005d
matsuzaki <shin.matsu.p@gmail.com>
2019-09-24 16:08:08 +0900

    おみくじ機能の実装

--- app01.py
+++ app01.py
@@ -1,4 +1,7 @@
+import random
+
 from flask import Flask
+import numpy as np
 
 app = Flask(__name__)
 
@@ -16,6 +19,14 @@ def goodbye():
 @app.route('/add/<a>/<b>')
 def add(a, b):
     return str(int(a) + int(b))
+
+
+@app.route('/omikuji')
+def omikuji():
+    results = ['大吉', '吉', '凶']
+    n = random.randint(0, len(results)-1)
+    # result = random.choice(results)
+    return f'{results[n]}'
 
 
 if __name__ == '__main__':
